405899f580d310d5dd9746f6575efff35ea13546
Claudio Santos Oliveira <monfredvoncastein@gmail.com>
2021-04-13 17:06:30 -0300

    Inclusion of adding categories to the form in the database

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/', function () {
 
 Route::get('/produtos', 'ControladorProduto@index');
 Route::get('/categorias', 'ControladorCategoria@index');
+Route::get('/categorias/novo', 'ControladorCategoria@create');
+Route::post('/categorias/novo', 'ControladorCategoria@store')->name('criar.categoria');
+
